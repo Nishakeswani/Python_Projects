8c6140795306437b8536136173330c5dabcbd59e
Nisha Keswani <39515414+Nishakeswani@users.noreply.github.com>
2023-07-13 04:19:15 +0530

    Update simple_calculator.py

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,1 +1,19 @@
 print("This is simple calculator")
+Number1 = int(input("Enter first Number:\n"))
+Number2 = int(input("Enter Second Number:\n"))
+
+#Addition
+Result = Number1 + Number2
+print(f"The sum of {Number1} and {Number2} is :  {Result}")
+
+#Subtraction
+Result = Number1 - Number2
+print(f"The Subtrcation of {Number1} and {Number2} is :  {Result}")
+
+#Multiplication
+Result = Number1 * Number2
+print(f"The Multiplication of {Number1} and {Number2} is :  {Result}")
+
+#Division
+Result = Number1 / Number2
+print(f"The Division of {Number1} and {Number2} is :  {Result}")
