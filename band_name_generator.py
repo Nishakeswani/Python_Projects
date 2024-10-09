416ab4f326401b698b0ca0759c3d2abf22beb02c
Nisha Keswani <39515414+Nishakeswani@users.noreply.github.com>
2024-10-09 21:19:21 +0530

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to band name generator")
+name1 = input("Enter the city name where you were born  ")
+name2 = input("Enter the name of your pet ")
+print("Your band name is :  "  + name1  + name2)
